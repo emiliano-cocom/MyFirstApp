029d9b55b06b025169f0a733251472f44b664908
Emiliano Cocom <joseemilianococom@gmail.com>
2022-07-10 12:21:22 -0500

    Property bindig

--- src/app/saludo/saludo.component.ts
+++ src/app/saludo/saludo.component.ts
@@ -8,12 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class SaludoComponent implements OnInit {
 
   message: string;
+  identifParrafo: string;
+  identifInput: string;
 
   constructor() { 
     this.message = 'Un saludo a todos';
+    this.identifParrafo = 'parrafoPrincipal';
+    this.identifInput = 'text';
 
     setTimeout( () => {
       this.message = 'Otro mensaje diferente';
+      this.identifParrafo = 'main';
+      this.identifInput = 'password';
     }, 3000);
 
   }
