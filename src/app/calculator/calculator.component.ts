76252c7017ff024184cc762d4ecb1964f1e2e54d
Emiliano Cocom <joseemilianococom@gmail.com>
2022-07-12 16:25:13 -0500

    ready calculator component

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
-  constructor() { }
+  result: string;
+  isFinishedCalculation: boolean;
+
+  constructor() {
+    this.result = '0';
+    this.isFinishedCalculation = false;
+  }
 
   ngOnInit(): void {
+  }
+
+  getNumber(value) {
+    if(this.isFinishedCalculation) {
+      this.result = '0';
+      this.isFinishedCalculation = false;
+    }
+
+    this.result === '0' ? this.result = value : this.result += value;
+  }
+
+  getResult() {
+    this.result = eval(this.result);
+    this.isFinishedCalculation = true;
   }
 
 }
