3f30601d9dca83c346dd133dbc6e8e170e18ec68
Emiliano Cocom <joseemilianococom@gmail.com>
2022-07-26 14:05:55 -0500

    Renderer in directives set styles to any plataform

--- src/app/directives/underline.directive.ts
+++ src/app/directives/underline.directive.ts
@@ -1,12 +1,15 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[underline]'
 })
 export class UnderlineDirective {
 
-  constructor(elem: ElementRef) {
-    elem.nativeElement.style.textDecoration = 'underline';
+  constructor(elem: ElementRef, renderer: Renderer2) {
+    // elem.nativeElement.style.textDecoration = 'underline';
+    /* renderer set style to any plataform */
+    renderer.setStyle(elem.nativeElement, 'textDecoration', 'underline');
+    renderer.setStyle(elem.nativeElement, 'color', 'indigo');
   }
 
 }
