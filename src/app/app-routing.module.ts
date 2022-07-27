055b36f9a755ad3a1b7bfa004c7402f57895963d
Emiliano Cocom <joseemilianococom@gmail.com>
2022-07-27 13:10:48 -0500

    Router Guardar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,10 +7,11 @@ import { EmployeeDetailComponent } from './components/employee-detail/employee-d
 import { ProjectsComponent } from './components/employees/projects/projects.component';
 import { ExperienciesComponent } from './components/employees/experiencies/experiencies.component'
 import { CurriculumComponent } from './components/employees/curriculum/curriculum.component';
+import { RandomGuard } from './random.guard';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'informacion'},
-  { path: 'informacion', component: InfoComponent },
+  { path: 'informacion', component: InfoComponent, canActivate: [RandomGuard] },
   { path: 'about', redirectTo: 'sobre' },
   { path: 'sobre', component: AboutComponent },
   { path: 'contacto', component: ContactComponent },
